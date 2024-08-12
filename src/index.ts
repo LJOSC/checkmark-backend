76c5fae88f34853afbed219ab2ccaed124046ce0
Harsh Dobariya <harshdobariya79@gmail.com>
2024-08-12 21:42:33 +0530

    Fix CORS origins

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ app.use(express.json({ limit: '10mb' }));
 
 // cors setup
 const corsOptions: CorsOptions = {
-  origin: ['*', 'http://localhost:5173'],
+  origin: [process.env.FRONTEND_HOST as string],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
 };
